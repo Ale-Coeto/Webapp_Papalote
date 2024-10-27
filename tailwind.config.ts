e3fb96e3d232c1b4c0da6f8d91b2b447b02888df
Oscar Arreola <oscar.arreola.jr@gmail.com>
2024-10-27 15:54:50 -0600

    feat: integrated shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,26 +2,72 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+    darkMode: ["class"],
+    content: ["./src/**/*.tsx"],
   theme: {
-    extend: {
-      colors : {
-        'verde': {
-          DEFAULT: '#12D354',
-          200: '#2DEA6D',
-        },
-        'gris': '#74787A',
-        'fondo': '#F4FAFE',
-        'texto': '#444444',
-        'azul': {
-          DEFAULT: '#36B4FE',
-          200: '#66C6FF',
-        }
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
-    },
+  	extend: {
+  		colors: {
+  			verde: {
+  				'200': '#2DEA6D',
+  				DEFAULT: '#12D354'
+  			},
+  			gris: '#74787A',
+  			fondo: '#F4FAFE',
+  			texto: '#444444',
+  			azul: {
+  				'200': '#66C6FF',
+  				DEFAULT: '#36B4FE'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			sans: ["var(--font-geist-sans)", ...fontFamily.sans]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
