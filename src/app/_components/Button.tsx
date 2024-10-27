852a627ba3111d9a8da31ff77e68d7809b9b1e8e
Alejandra Coeto <a01285221@tec.mx>
2024-10-27 16:02:08 -0600

    Finished special events page. Can add, update or delete events

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -4,12 +4,17 @@ interface ButtonProps {
     secondary?: boolean;
     full?: boolean;
     submit?: boolean;
+    danger?: boolean;
 }
 
-export default function Button({label, onClick, secondary, full, submit} : ButtonProps) {
+export default function Button({label, onClick, secondary, full, submit, danger} : ButtonProps) {
     return (
         <div className="flex justify-end">
-            <button type={submit ? "submit" : "button"} onClick={onClick} className={`bg-verde hover:bg-verde-200 text-white font-semibold py-2 px-4 rounded ${secondary ? "bg-slate-400 hover:bg-slate-300" : ""} ${full && "w-full"}`}>
+            <button type={submit ? "submit" : "button"} onClick={onClick} 
+            className={`text-white font-semibold py-2 px-4 rounded 
+            ${secondary ? "bg-slate-400 hover:bg-slate-300" : "bg-verde hover:bg-verde-200"} 
+            ${danger && "bg-red-500 hover:bg-red-400"}
+            ${full && "w-full"}`}>
                 {label}
             </button>
         </div>
--- src/app/_components/eventosEspeciales/NewEventModal.tsx
+++ src/app/_components/eventosEspeciales/NewEventModal.tsx
@@ -1,13 +1,29 @@
 import Button from "../Button";
 import AddButton from "../forms/AddButton";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { api } from "~/trpc/react";
 
 
 export default function NewEventModal({ onClose }: { onClose: () => void }) {
     const { handleSubmit, register, setValue, formState: { errors } } = useForm<FieldValues>({ defaultValues: { title: '', content: '' } });
+    const addEvent = api.specialEventRouter.createSpecialEvent.useMutation();
 
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        console.log(data);
+
+        addEvent.mutate({
+            name: data.nombre,
+            description: data.descripcion,
+            startDate: new Date(data.fechaInicio),
+            endDate: new Date(data.fechaFin),
+            image: "data.infografia"
+        });
+
+        setValue("nombre", "");
+        setValue("descripcion", "");
+        setValue("fechaInicio", "");
+        setValue("fechaFin", "");
+        setValue("infografia", "");
+
         onClose();
     }
 
@@ -17,27 +33,27 @@ export default function NewEventModal({ onClose }: { onClose: () => void }) {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col pb-4 w-full">
                     <label>Nombre del evento</label>
-                    <input type="text" className="border-2 rounded-md px-1 w-full" {...register("nombre")} required />
+                    <input id="nombre" type="text" className="border-2 rounded-md px-1 w-full" {...register("nombre")} required />
                 </div>
 
                 <div className="flex flex-col pb-6">
                     <label>Fecha Inicio</label>
-                    <input type="date" className="border-2 rounded-md px-1" {...register("fechaInicio")} required/>
+                    <input id="fechaInicio" type="date" className="border-2 rounded-md px-1" {...register("fechaInicio")} required/>
                 </div>
 
                 <div className="flex flex-col pb-6">
                     <label>Fecha Fin</label>
-                    <input type="date" className="border-2 rounded-md px-1" {...register("fechaFin")} required/>
+                    <input id="fechaFin" type="date" className="border-2 rounded-md px-1" {...register("fechaFin")} required/>
                 </div>
 
                 <div className="flex flex-col pb-6">
                     <label>Descripción</label>
-                    <textarea className="border-2 rounded-md px-1" {...register("descripcion")} required/>
+                    <textarea id="descripcion" className="border-2 rounded-md px-1" {...register("descripcion")} required/>
                 </div>
 
                 <div className="flex flex-col pb-6">
                     <label>Infografía</label>
-                    <input type="file" className="" accept=".jpg, .jpeg, .png" {...register("infografia")} />
+                    <input id="infografia" type="file" className="" accept=".jpg, .jpeg, .png" {...register("infografia")} />
                 </div>
 
                 <div className="flex flex-col pb-6">
